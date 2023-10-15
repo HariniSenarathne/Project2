8cb72bfe22b6d91b31fce9eef1ce20f20bae3fcf
Chamathka <chamathka15644@gmail.com>
2023-10-15 15:54:09 +0530

    Added new code to find the smallest number

--- Large number.py
+++ Large number.py
@@ -12,3 +12,19 @@ else:
    largest = num3
 
 print("The largest number is", largest)
+
+
+
+# Find the smallest number among the five input numbers
+# Input five numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+num4 = float(input("Enter the fourth number: "))
+num5 = float(input("Enter the fifth number: "))
+
+# Find the smallest number
+smallest = min(num1, num2, num3, num4, num5)
+
+# Print the result
+print(f"The smallest number among {num1}, {num2}, {num3}, {num4}, and {num5} is {smallest}")
